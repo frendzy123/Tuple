6b0ab873860b109d8f88195e26bf09d85e26f0c5
Farhan Alam <farhan.alam202@gmail.com>
2019-01-18 16:49:54 -0800

    Learning Controls

---
+++ Tuple/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerControls : MonoBehaviour {
+
+    public SteamVR_Action_Vector2 touchpadPos;
+    public SteamVR_Action_Boolean touchpadClick;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (touchpadClick) {
+            if (touchpadPos.x > 0.5)
+            {
+                Debug.Log("Right");
+            }
+            else if(touchpadPos.x <= 0.5){
+                Debug.Log("Left");
+            }
+        }
+	}
+}
