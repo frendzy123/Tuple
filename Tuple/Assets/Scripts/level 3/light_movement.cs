0445cc65853b391a19e971e24421b1502127aa5e
Eugene <omarbot@gmail.com>
2019-05-28 17:30:02 -0700

    05/28/19 05:29PM
    game lab

--- Tuple/Assets/Scripts/level 3/light_movement.cs
+++ Tuple/Assets/Scripts/level 3/light_movement.cs
@@ -7,14 +7,16 @@ public class light_movement : MonoBehaviour
 	private Quaternion targetAngle0 = Quaternion.Euler(52.641f,71.232f,66.853f);
 	private Quaternion targetAngle1 = Quaternion.Euler(51.783f,-71.82301f,-67.32f);
 	private int flip = 0;
-	public float speed = 0.01f;
-	public int flickerIntervalSec = 7;
 	private float prevTime;
-	public float elapsedTime = 0f; //change private
+	private int elapsedTime = 1;
 	private Light lightComp;
 
-	public float flickerwait = 0.8f;
-	public float flickerpause = 5f;
+	public float lightRotationSpeed = 0.01f;
+	public int flickerIntervalSec = 7;
+	public float flickerStutterInterval = 0.1f;
+	public float flickerPauseInterval = 7f;
+	public bool isFlickerActive = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,58 +27,77 @@ public class light_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	elapsedTime = elapsedTime + (Time.time - prevTime);
-    	elapsedTime = Mathf.Round(Mathf.Repeat(elapsedTime, flickerIntervalSec));
-    	prevTime = Time.time;
-
-    	if (elapsedTime % flickerIntervalSec == 0) {
-    		StartCoroutine(flicker());
-    	}
-    	else {
-    		if (flip == 0) {
-	    		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetAngle1, speed);
+    	if (isFlickerActive == false) {
+	    	if (elapsedTime % flickerIntervalSec == 0) {
+	    		elapsedTime = 1;
+	    		StartCoroutine(flicker());
+	    	}
+	    	if (Mathf.Round(Time.time - prevTime) == 1) {
+	    		elapsedTime = (elapsedTime + 1) % flickerIntervalSec;
+	    		prevTime = Time.time;
+	    	}
+			if (flip == 0) {
+	    		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetAngle1, lightRotationSpeed);
 	    		if (this.transform.rotation == targetAngle1) {
 	    			flip = 1;
 	    		}
-    		}
-	    	else {
-	    		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetAngle0, speed);
+			}
+	    	else if (flip == 1) {
+	    		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetAngle0, lightRotationSpeed);
 	    		if (this.transform.rotation == targetAngle0) {
 	    			flip = 0;
 	    		}
 	    	}
-    	}	
+    	}
     }
 
     IEnumerator flicker() {
+    	isFlickerActive = true;
     	lightComp.intensity = 5f;
 
-    	lightComp.enabled = !lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
-    	lightComp.enabled = lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
-/*
-    	lightComp.enabled = !lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
-    	lightComp.enabled = lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
 
-    	lightComp.enabled = !lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
-    	lightComp.enabled = lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
 
-    	lightComp.enabled = !lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
-    	lightComp.enabled = lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerwait);
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
 
     	//big break
-    	lightComp.enabled = !lightComp.enabled;
-    	yield return new WaitForSecondsRealtime(flickerpause);
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerPauseInterval);
 
-    	lightComp.enabled = lightComp.enabled;
-*/
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+    	lightComp.enabled = true;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+
+    	lightComp.enabled = false;
+    	yield return new WaitForSecondsRealtime(flickerStutterInterval);
+
     	lightComp.intensity = 16f;
+    	lightComp.enabled = true;
+    	isFlickerActive = false;
+    	prevTime = Time.time;
     }
 }
