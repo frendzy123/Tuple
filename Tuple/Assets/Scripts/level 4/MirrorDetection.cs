464815ee23d57f1338d0de3f5c76d87239d8b824
Farhan Alam <farhan.alam202@gmail.com>
2019-06-11 20:22:15 -0700

    Level4 mechanics done except for the eyes

---
+++ Tuple/Assets/Scripts/level 4/MirrorDetection.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorDetection : MonoBehaviour
+{
+
+    public GameObject _head;
+    public Level4Score _playerScore;
+
+    private Vector3 _mirrorHeadPosition = Vector3.zero;
+    private float _accumulatedPoints = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector3(-_head.transform.position.x, _head.transform.position.y, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _mirrorHeadPosition = collision.gameObject.transform.position;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        float distance = Vector3.Distance(_mirrorHeadPosition, collision.gameObject.transform.position);
+        if (distance < 0.6) {
+            distance = 0;
+        }
+        Debug.Log(distance);
+        _playerScore._playerScore +=  0.1f * distance;
+        _mirrorHeadPosition = collision.gameObject.transform.position;
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        _accumulatedPoints = 0;
+    }
+}
---
+++ Tuple/Assets/Scripts/level 4/Mouth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouth : MonoBehaviour
+{
+
+    public Level4Score _playerScore;
+    public Sprite neutral;
+    public Sprite smile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerScore._playerScore > 35f && _playerScore._playerScore < 80f)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = neutral;
+        }
+        else if (_playerScore._playerScore > 80f)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = smile;
+        }
+    }
+}
